c59ca3b773368b2c0f958fb14f5bece991cb2ebf
harsha kapila <harshakapila87@gmail.com>
2021-12-01 11:01:34 +0530

    commiting updated work

---
+++ IdeaProjects/cucumber/src/main/java/Apache_POI/abc.java
@@ -0,0 +1,4 @@
+package cucumber.src.main.java.Apache_POI;
+
+public class abc {
+}
---
+++ IdeaProjects/sample/src/test/java/PrioritizeDisablingEx.java
@@ -0,0 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrioritizeDisablingEx {
+    @Test(priority = 1,enabled=false)
+    void setup()
+    {
+        System.out.println("Opening browser");
+       // Assert.fail();
+    }
+
+    @Test(priority = 2)
+    void login() {
+        System.out.println("This is login test");
+    }
+
+    @Test(priority = 3)
+    void teardown() {
+        System.out.println("Closing a browser");
+    }
+}
